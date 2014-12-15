6660c7e15d115cb46b82300ecb979ee3614f7a1a
bueltge <frank@bueltge.de>
2014-12-15 12:40:26 +0100

    enhance unit gimmicks

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,3 +4,8 @@ namespace Multisite_Enhancements;
 
 // Use default composer autoload
 require_once dirname( __DIR__ ) . '/vendor/autoload.php';
+
+// Activates this plugin in WordPress so it can be tested.
+$GLOBALS[ 'wp_tests_options' ] = array(
+	'active_plugins' => array( 'wordpress-multisite-enhancements/multisite-enhancements.php' ),
+);
--- tests/test-gimmick.php
+++ tests/test-gimmick.php
@@ -7,9 +7,52 @@ class Test_Gimmick extends \PHPUnit_Framework_TestCase {
 	/**
 	 * Validate string
 	 */
-	public function test_string() {
+	public function right_test_string() {
+
+		$string = 'I will learn UnitTesting';
+		$this->assertEquals( 'I will learn UnitTesting', $string );
+	}
+
+	public function wrong_test_string() {
 
 		$string = 'I will learn UnitTesting';
 		$this->assertEquals( 'I will learn UnitTesting, now', $string );
+	}
+}
+
+/**
+ * Tests to test that that testing framework is testing tests. Meta, huh?
+ *
+ * @package wordpress-plugins-tests
+ */
+class WP_Test_WordPress_Plugin_Tests extends \WP_UnitTestCase {
+
+	/**
+	 * Run a simple test to ensure that the tests are running
+	 */
+	function test_tests() {
+
+		$this->assertTrue( TRUE );
+	}
+
+	/**
+	 * If these tests are being run on Travis CI, verify that the version of
+	 * WordPress installed is the version that we requested.
+	 */
+	function test_wp_version() {
+
+		if ( ! getenv( 'TRAVIS_PHP_VERSION' ) ) {
+			$this->markTestSkipped( 'Test skipped since Travis CI was not detected.' );
+		}
+		//grab the requested version
+		$requested_version = getenv( 'WP_VERSION' );
+		//trunk is always "master" in github terms, but WordPress has a specific way of describing it
+		//grab the exact version number to verify that we're on trunk
+		if ( 'master' === $requested_version ) {
+			$file = file_get_contents( 'https://raw.github.com/WordPress/WordPress/master/wp-includes/version.php' );
+			preg_match( '#\$wp_version = \'([^\']+)\';#', $file, $matches );
+			$requested_version = $matches[ 1 ];
+		}
+		$this->assertEquals( get_bloginfo( 'version' ), $requested_version );
 	}
 }
