f7ea6ca3e5a222a411c547fbf5f769ee7e610afa
bueltge <frank@bueltge.de>
2014-12-15 16:32:52 +0100

    enhance bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,3 +9,14 @@ require_once dirname( __DIR__ ) . '/vendor/autoload.php';
 $GLOBALS[ 'wp_tests_options' ] = array(
 	'active_plugins' => array( 'wordpress-multisite-enhancements/multisite-enhancements.php' ),
 );
+
+// If the wordpress-tests repo location has been customized (and specified
+// with WP_TESTS_DIR), use that location. This will most commonly be the case
+// when configured for use with Travis CI.
+// Otherwise, we'll just assume that this plugin is installed in the WordPress
+// SVN external checkout configured in the wordpress-tests repo.
+if ( FALSE !== getenv( 'WP_TESTS_DIR' ) ) {
+	require getenv( 'WP_TESTS_DIR' ) . '/bootstrap.php';
+} else {
+	require dirname( dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) ) . '/bootstrap.php';
+}
