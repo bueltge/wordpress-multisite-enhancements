304154d455767d119394ecd2c9b9d0fe61d542fd
bueltge <frank@bueltge.de>
2014-12-16 11:20:34 +0100

    retest travis with new path

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,20 +3,18 @@
 #namespace Multisite_Enhancements;
 
 // Use default composer autoload
-require_once dirname( __DIR__ ) . '/vendor/autoload.php';
+#require_once dirname( __DIR__ ) . '/vendor/autoload.php';
 
 // Activates this plugin in WordPress so it can be tested.
 $GLOBALS[ 'wp_tests_options' ] = array(
 	'active_plugins' => array( 'wordpress-multisite-enhancements/multisite-enhancements.php' ),
 );
 
-// If the wordpress-tests repo location has been customized (and specified
-// with WP_TESTS_DIR), use that location. This will most commonly be the case
-// when configured for use with Travis CI.
-// Otherwise, we'll just assume that this plugin is installed in the WordPress
-// SVN external checkout configured in the wordpress-tests repo.
-if ( FALSE !== getenv( 'WP_TESTS_DIR' ) ) {
-	require getenv( 'WP_TESTS_DIR' ) . '/includes/bootstrap.php';
+// If the develop repo location is defined (as WP_DEVELOP_DIR), use that
+// location. Otherwise, we'll just assume that this plugin is installed in a
+// WordPress develop SVN checkout.
+if ( FALSE !== getenv( 'WP_DEVELOP_DIR' ) ) {
+	require getenv( 'WP_DEVELOP_DIR' ) . '/tests/phpunit/includes/bootstrap.php';
 } else {
-	require dirname( dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) ) . '/includes/bootstrap.php';
+	require '../../../../tests/phpunit/includes/bootstrap.php';
 }
