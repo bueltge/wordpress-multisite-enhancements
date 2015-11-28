c1932578e41a1133c0f98a99122608795a8b01c4
bueltge <frank@bueltge.de>
2015-11-28 16:30:44 +0100

    format js source

--- inc/assets/js/filtering-plugins.js
+++ inc/assets/js/filtering-plugins.js
@@ -2,17 +2,18 @@
 // extend :contains to be case-insensitive; via http://stackoverflow.com/questions/187537/
 jQuery.expr[ ':' ].contains = function( a, i, m ) {
 	return (
-		a.textContent || a.innerText || ""
+			a.textContent || a.innerText || ""
 		).toUpperCase().indexOf( m[ 3 ].toUpperCase() ) >= 0;
 };
 jQuery( document ).ready( function( $ ) {
 	$( '#plugin-search-input' ).keyup( function() {
 		$val = $( this ).val();
+		$selector = '#the-list';
 		if ( $val.length < 2 ) {
-			$( "#the-list").find("> tr" ).show();
+			$( $selector ).find( "> tr" ).show();
 		} else {
-			$( "#the-list").find("> tr" ).hide();
-			$( "#the-list").find(".plugin-title strong:contains(" + $val + ")" ).parent().parent().show();
+			$( $selector ).find( "> tr" ).hide();
+			$( $selector ).find( ".plugin-title strong:contains(" + $val + ")" ).parent().parent().show();
 		}
 	} ).focus();
 } );
