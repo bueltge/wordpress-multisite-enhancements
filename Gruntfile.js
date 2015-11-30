9c73910101e86aad79bd767bd2409192412db5a9
bueltge <frank@bueltge.de>
2015-11-30 16:33:06 +0100

    Enhance grunt config to exclude minified js for jscs checks.

--- Gruntfile.js
+++ Gruntfile.js
@@ -49,7 +49,7 @@ module.exports = function( grunt ) {
 				src: [ 'Gruntfile.js' ]
 			},
 			scripts: {
-				src: [ '<%= config.scripts.src %>**/*.js' ]
+				src: [ '<%= config.scripts.src %>**/*.js', '!<%= config.scripts.src %>**/*.min.js' ]
 			}
 		},
 
