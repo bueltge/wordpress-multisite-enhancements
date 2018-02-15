37ec8a49c01e00b81eeda24a4004c8ff80fd4065
bueltge <frank@bueltge.de>
2018-02-15 15:31:36 +0100

    Reformat for codex WP

--- inc/autoload/class-add-css.php
+++ inc/autoload/class-add-css.php
@@ -3,10 +3,10 @@
  * On the network plugin and theme pages, add css to present the active column
  * If this class is loaded, modify the presentation of the column in order to
  * allow showing or hiding the list of sites that uses a theme or plugin
- * 
  *
- * @since   1.4.2
- * @version 1.4.2
+ *
+ * @since   2018-02-15
+ * @version 2018-02-15
  * @package WordPress
  */
 
@@ -22,6 +22,16 @@ add_action( 'init', array( 'Enqueue_Column_Style', 'init' ) );
 class Enqueue_Column_Style {
 
 	/**
+	 * Init function to register all used hooks.
+	 *
+	 */
+	public function __construct() {
+
+		add_action( 'admin_head-themes.php', array( $this, 'enqueue_style' ) );
+		add_action( 'admin_head-plugins.php', array( $this, 'enqueue_style' ) );
+	}
+
+	/**
 	 * Initialize the class.
 	 */
 	public static function init() {
@@ -33,27 +43,18 @@ class Enqueue_Column_Style {
 	}
 
 	/**
-	 * Init function to register all used hooks.
-	 *
-	 */
-	public function __construct() {
-		add_action( 'admin_head-themes.php', array( $this, 'enqueue_style' ) );
-		add_action( 'admin_head-plugins.php', array( $this, 'enqueue_style' ) );
-	}
-
-    /**
 	 * Enqueue column style.
-     * 
+	 *
 	 */
 	public function enqueue_style() {
 
 		$suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
-        
-        wp_register_style(
-            'admin_column_css', 
-            plugins_url( '/inc/assets/css/wordpress-multisite-enhancements' . $suffix . '.css', MULTISITE_ENHANCEMENT_BASE ),
-             false);
-        wp_enqueue_style( 'admin_column_css' );
-    } // end enqueue_style()
-    
+
+		wp_register_style(
+			'admin_column_css',
+			plugins_url( '/inc/assets/css/wordpress-multisite-enhancements' . $suffix . '.css', MULTISITE_ENHANCEMENT_BASE ),
+			false );
+		wp_enqueue_style( 'admin_column_css' );
+	} // end enqueue_style()
+
 } // end class
