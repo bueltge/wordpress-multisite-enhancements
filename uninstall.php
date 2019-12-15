ef85eb2abef5e6c0e56136f85b5c436dbe9f29d1
bueltge <frank@bueltge.de>
2019-12-15 16:56:53 +0100

    keep simple the deinstallation of the option data

--- uninstall.php
+++ uninstall.php
@@ -1,32 +1,10 @@
 <?php
 /**
  * Uninstallation script
- * This file is called automatically by WP when the admin deletes the plugin from the network
+ * This file is called automatically by WP when the admin deletes the plugin from the network.
  */
 
-// if not called by WordPress, die
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-	die;
-}
+! defined( 'WP_UNINSTALL_PLUGIN' ) && exit;
 
-class Multisite_Enhancements_Uninstall {
-
-	public function __construct() {
-		self::uninstall();
-	}
-
-	/**
-	 * Delete database option when plugin is uninstalled
-	 */
-	public static function uninstall() {
-
-		require_once __DIR__ . '/settings.php';
-
-		if ( Multisite_Enhancements_Settings::is_feature_enabled( 'delete-settings' ) ) {
-			delete_site_option( Multisite_Enhancements_Settings::OPTION_NAME );
-		}
-	}
-
-}
-
-new Multisite_Enhancements_Uninstall();
+require_once __DIR__ . 'src/settings.php';
+delete_site_option( Multisite_Enhancements_Settings::OPTION_NAME );
